dadf95e80d25450ded7d9d567d334e37200dd96e
Neo <Neo@192.168.43.104>
2013-09-09 01:57:16 +0300

    index.php Fixes

--- index.php
+++ index.php
@@ -1,98 +1,119 @@
 <?php include('template/header.php'); ?>
+<title>Home Page</title>
 <?php include('template/left_pane.php');?>      
-    <div class='span6' id='content'>
+    <div class="row">
+	<div class='span9' id='content'>
 
         <?php include('modules/image_slider.php'); ?>
-
         <div id="welcome_msg" class="span9">
             <div class="title_bar">Welcome to The Foundation of Sheikhs and Islamic Scholars of Tanzania</div><!-- title_bar -->
             <div class="msg_body">
-                Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Donec ullamcorper nulla non metus auctor fringilla. Maecenas faucibus mollis interdum. Donec ullamcorper nulla non metus auctor fringilla.
-                <button type="submit" class="btn  btn-mini btn-inverse">Read More</button>
+                Islam and Christianity are the two main religions with big number of believers in Tanzania and they are the main religions in which political, public leaders and most businessmen come from.
+                <a href="about.php"><button type="submit" class="btn  btn-mini btn-inverse">Read More</button></a>
             </div><!-- msg_body-->
         </div><!-- welcome_msg -->
-
-    </div><!-- content -->
-
-    <div class='row'>
-        <div class='span4'>
-            <div id="new_scholar_form" class="span4">
-                <div class="title_bar_brown">Add an Islamic Scholar / Sheikh</div><!-- title_bar -->
+		
+		<!--<div id="join_forum" class="span9">
+                <div class="title_bar_brown">Join our Discussion Area Today</div>
                 <div class="msg_body">
-                    <form name="new_scholar_form" action="" method='post'>
-                        <input class="input-xlarge" type="text" placeholder="Sheikh Ibn Kathir" />
-                        <button type="submit" class="btn btn-success offset2">Add</button>
-                    </form>
+                    This is our discussion area
                 </div>
-            </div><!-- new_scholar_form -->
-
-            <div id="recent_scholars" class="span4">
+        </div>-->
+		
+		<div id="recent_scholars" class="span6">
                 <div class="title_bar_brown">Recently Added Scholars / Sheikhs</div><!-- title_bar -->
                 <div class="msg_body">
                     <table class='table table-condensed table-hover'>
-                        <tr>
-                            <td>7th July 2013</td>
-                            <td>Anwar Al Awlaki</td>
-                            <td><button type="submit" class="btn btn-small btn-inverse">View</button></td>
-                        </tr>
-                        <tr>
-                            <td>7th July 2013</td>
-                            <td>Ibn Kathir</td>
-                            <td><button type="submit" class="btn btn-small btn-inverse">View</button></td>
-                        </tr>
-                        <tr>
-                            <td>7th July 2013</td>
-                            <td>Ibn Taimia</td>
-                            <td><button type="submit" class="btn btn-small btn-inverse">View</button></td>
-                        </tr>
+						<?php
+							$q = mysql_query("SELECT * FROM  `h_scholartb` ORDER BY  `sclar_reg_date` DESC LIMIT 4");
+							if(mysql_num_rows($q) > 0){
+								while($row = mysql_fetch_array($q)){
+									$row['sclar_reg_date'] = date('jS M, Y', strtotime($row['sclar_reg_date']));
+									$date = $row['sclar_reg_date'];
+									$scholar_name = $row['sclar_name'];
+									$id = $row['sclar_id'];
+									echo "<tr><td width='25%' style='font-weight:bold;'>$date</td><td width='65%'>$scholar_name</td><td width='10%'><button type='submit' class='btn btn-small btn-inverse'>View</button></td></tr>";
+								}
+							}
+							else{
+								echo "<tr><td></td><td>No Recently Added Scholars / Sheikhs</td><td></td></tr>";
+							}
+                        ?>
                     </table>
                 </div><!-- msg_body -->
-            </div><!-- recent_scholars --> 
-
-            <div class="span9">
-                <br />
-                <div class="msg_body">
-                    <div class="input-append">
-                        <input class="input-xxlarge" id="appendedInputButton" type="text" placeholder='Type your Question Here'>
-                        <button class="btn btn-success" type="button">Ask a Question</button>
-                    </div>
-                </div>
-            </div><!-- questions -->
-
-            <div class='span9'>
+            </div><!-- recent_scholars -->
+			
+			<?php
+				$msg = "";
+				if(isset($_POST['send'])){
+					$ques = $_POST['ques'];
+					if ($ques != "") {
+						$sqlCommand = "INSERT INTO h_querytb VALUES('', '$id','$ques',now(),'','','','no')";  
+						$query = mysql_query($sqlCommand) or die (mysql_error());
+						$msg = "<span style='color:green;'>Jazakallahu Khairan for your contribution.</span>";
+						header ("Location: questions.php");
+					}
+					else{
+						$msg = "<span style='color:red;'>Please type in your Question.</span>";
+					}
+				}
+				else{
+					
+				}
+			?>
+			<?php
+				if(!isset($_SESSION["userlogin"])){
+					echo "";
+				}
+				else{
+					echo "<div class='span9'>
+						<br />
+						<div class='title_bar_brown'>ASK A QUESTION</div>
+						<div class='msg_body'>
+							<div class='input-append'>
+							<form action='' method='post' enctype='multipart/form-data'>
+								<textarea placeholder='Type your Question' rows='1' style='width: 554px; height: 45px;' wrap='hard' name='ques'></textarea>
+								<button class='btn btn-success' type='submit' name='send'>Ask a Question</button>
+								</form>
+							</div>
+							<span>$msg</span>
+						</div>
+					</div>";
+				}
+			?>
+			<div class='span9' style="margin-top:10px;">
                 <div class='msg_body'>
                     <table class='table table-condensed table-hover'>
-                        <tr>
-                            <td>1</td>
-                            <td>Oh Sheikh enlighten me on how I should pray tahajjud?</td>
-                            <td><button type="submit" class="btn btn-small btn-inverse">View Answer</button></td>
-                        </tr>
-                        <tr>
-                            <td>2</td>
-                            <td>Oh Sheikh enlighten me on how I should pray tahajjud?</td>
-                            <td><button type="submit" class="btn btn-small btn-inverse">View Answer</button></td>
-                        </tr>
-                        <tr>
-                            <td>3</td>
-                            <td>Oh Sheikh enlighten me on how I should pray tahajjud?</td>
-                            <td><button type="submit" class="btn btn-small btn-inverse">View Answer</button></td>
-                        </tr>
+						<?php
+							$q = mysql_query("SELECT * FROM  `h_querytb` ORDER BY  `qu_id` DESC LIMIT 4");
+							if(mysql_num_rows($q) > 0){
+								while($row = mysql_fetch_array($q)){
+									$row['qu_date'] = date('jS M, Y', strtotime($row['qu_date']));
+									$date = $row['qu_date'];
+									$content = $row['qu_content'];
+									$id = $row['qu_id'];
+									$quanswered = $row['qu_answered'];
+									$msg = "";
+									if($quanswered == 'yes')
+									{
+										$msg = "<a href='answers.php?id=$id'><button type='submit' class='btn btn-small btn-inverse'>View Answer</button></a>";
+									}
+									else{
+										$msg = "<button class='btn btn-small btn-inverse'>Not Answered</button>";
+									}
+									echo "<tr><td width='15%' style='font-weight:bold;'>$date</td><td width='67%'>$content</td><td width='18%' style='text-align:right;'>$msg</td></tr>";
+								}
+							}
+							else{
+								echo "<tr><td></td><td>No Questions</td><td></td></tr>";
+							}
+                        ?>
                     </table>
                 </div>
             </div>
 
-        </div>
-
-        <div class='span5'>
-            <div id="join_forum" class="span5">
-                <div class="title_bar_brown">Join our Discussion Area Today</div><!-- title_bar -->
-                <div class="msg_body">
-                    This is our discussion area
-                </div>
-            </div><!-- join_forum -->     
-        </div>
     </div>
-</div><!-- row [main_content] -->
+	</div><!-- content -->
 <br />
 <?php include('template/footer.php');?> 
 <?php include('template/sponsors.php');?>
