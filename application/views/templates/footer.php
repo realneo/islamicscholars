ed089e98bb8f86b1a8e224800ae69e64e1ea3bed
Nadhir Bahayan <nbahayan@gmail.com>
2013-12-22 15:35:48 +0300

    Footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -3,14 +3,17 @@
 		</div><!-- container -->
         <br />
         <br />
-        <div class="container-fluid opacity-9">
-            <div class="container">
+        <div class="container-fluid">
+            <div class="container opacity-5">
                 <div class='row'>
-                    <?php
-                    echo anchor('site/index', 'Home')
-                ?>
+                    <div class='span12 white lighter text-center font-12'>All Rights Reserved. The Foundation of Sheikhs and Islamic Scholars in Tanzania 2013. Developed by <a href='http://www.yoteyote.com' alt='Yoteyote'>www.yoteyote.com</a></div>
+                    <div class='span12 text-center white lighter font-13'><h5>Sponsors</h5></div>
+                    <div class='span3'><?php echo img('assets/img/amana_bank.gif', 'Amana Bank') ?></div>
+                    <div class='span3'><?php echo img('assets/img/iou.gif', 'Islamic Online University') ?></div>
+                    <div class='span3'><?php echo img('assets/img/hajjtrust.gif', 'Hajj Trust') ?></div>
+                    <div class='span3'><?php echo img('assets/img/samssuper.gif', 'Sams Super Store') ?></div>
                 </div>
-                
+                <div class='span12'><br /></div>
             </div>
         </div>
 	</body>
