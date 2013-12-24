9f0ecc32b79bb63b1efae27a863f79ae882111d2
UltiGeek <feisal@yoteyote.com>
2013-12-24 21:39:12 +0300

    added icons to all libraries

--- application/views/library.php
+++ application/views/library.php
@@ -19,7 +19,7 @@
                                 <div class="heading2 padding-all-5"><?php echo $article->lib_title; ?></div>
                                 <p class='lighter font-12'><?php echo $article->lib_desc; ?></p>
                                 <span class='lighter font-13'><?php echo $article->lib_author . " | " . $article->lib_date;?></span>
-                                <a href="<?php echo $article->lib_file; ?>" class="btn btn-default btn-small pull-right margin-bottom-5 "> Download</a>
+                                <a href="<?php echo $article->lib_file; ?>" class="btn btn-default btn-small pull-right margin-bottom-5 margin-bottom-5"> Download</a>
                             </div>
                         </div>
                         <div class='span9'></div>
@@ -28,16 +28,16 @@
                 </div>
                 
                 <div class="tab-pane" id="tab2">
-                    <div class="title_bar_white span9">Library | Books</div>
+                    <div class="title_bar_white ">Library | Books</div>
                         <?php foreach($books as $book): ?>
                     <div class='row'>
                         <div class="opacity-5 span9">
-                            <div class="span1"><img src="img/library/books_icon.png" alt="" /></div>
+                            <div class="span1 padding-all-10"><img src="http://localhost/islamicscholars/assets/img/library/books_icon.png" alt="" /></div>
                             <div class="span7 white">
-                                <div class="heading2"><?php echo $book->lib_title; ?></div>
+                                <div class="heading2 padding-all-5"><?php echo $book->lib_title; ?></div>
                                 <p class='lighter font-12'><?php echo $book->lib_desc; ?></p>
                                 <span class='lighter font-13'><?php echo $book->lib_author . " | " . $book->lib_date;?></span>
-                                <a href="<?php echo $book->lib_file; ?>" class="btn btn-default btn-small pull-right"> Download</a>
+                                <a href="<?php echo $book->lib_file; ?>" class="btn btn-default btn-small pull-right margin-bottom-5"> Download</a>
                             </div>
                         </div>
                         <div class='span9'></div>
@@ -46,16 +46,16 @@
                 </div>
                 
                 <div class="tab-pane" id="tab3">
-                    <div class="title_bar_white span9">Library | Audios</div>
+                    <div class="title_bar_white ">Library | Audios</div>
                         <?php foreach($audios as $audio): ?>
                     <div class='row'>
                         <div class="opacity-5 span9">
-                            <div class="span1"><img src="img/library/audios_icon.png" alt="" /></div>
+                            <div class="span1 padding-all-10"><img src="http://localhost/islamicscholars/assets/img/library/audio_icon.png" alt="" /></div>
                             <div class="span7 white">
-                                <div class="heading2"><?php echo $audio->lib_title; ?></div>
+                                <div class="heading2 padding-all-5"><?php echo $audio->lib_title; ?></div>
                                 <p class='lighter font-12'><?php echo $audio->lib_desc; ?></p>
                                 <span class='lighter font-13'><?php echo $audio->lib_author . " | " . $audio->lib_date;?></span>
-                                <a href="<?php echo $audio->lib_file; ?>" class="btn btn-default btn-small pull-right"> Download</a>
+                                <a href="<?php echo $audio->lib_file; ?>" class="btn btn-default btn-small pull-right margin-bottom-5"> Download</a>
                             </div>
                         </div>
                         <div class='span9'></div>
@@ -64,16 +64,16 @@
                 </div>
                 
                 <div class="tab-pane" id="tab4">
-                    <div class="title_bar_white span9">Library | Videos</div>
+                    <div class="title_bar_white ">Library | Videos</div>
                         <?php foreach($videos as $video): ?>
                     <div class='row'>
                         <div class="opacity-5 span9">
-                            <div class="span1"><img src="img/library/videos_icon.png" alt="" /></div>
+                            <div class="span1 padding-all-10"><img src="http://localhost/islamicscholars/assets/img/library/videos_icon.png" alt="" /></div>
                             <div class="span7 white">
-                                <div class="heading2"><?php echo $video->lib_title; ?></div>
+                                <div class="heading2 padding-all-5"><?php echo $video->lib_title; ?></div>
                                 <p class='lighter font-12'><?php echo $video->lib_desc; ?></p>
                                 <span class='lighter font-13'><?php echo $video->lib_author . " | " . $video->lib_date;?></span>
-                                <a href="<?php echo $video->lib_file; ?>" class="btn btn-default btn-small pull-right"> Download</a>
+                                <a href="<?php echo $video->lib_file; ?>" class="btn btn-default btn-small pull-right margin-bottom-5"> Download</a>
                             </div>
                         </div>
                         <div class='span9'></div>
