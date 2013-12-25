171b90d4f08fd4636478bb091b6a34e3ed9e7be8
UltiGeek <feisal@yoteyote.com>
2013-12-25 18:47:20 +0300

    events page looking good

--- application/views/event_item.php
+++ application/views/event_item.php
@@ -2,10 +2,12 @@
     <?php foreach($event_item as $event): ?>
     <div class="row">
         <div class="title_bar_white span9"><?php echo "ID: ". $event->evt_id . " | ". $event->evt_date; ?></div>
-        <div class="span9 opacity-9 white heading1 padding-all-5"><?php echo $event->evt_name; ?></div>
-        <div class='span9 text-center opacity-5'><?php echo img("assets/img/events/$event->evt_img"); ?></div>
-        <div class="span9 opacity-7 white padding-all-5"><?php echo $event->evt_venue; ?></div>
-        <div class="span9 opacity-5 white padding-all-5"><?php echo $event->evt_desc; ?></div>
+        <div class="span9 opacity-9 white heading1 padding-all-10"><?php echo $event->evt_name; ?></div>
+        <div class='span9 text-center opacity-5 padding-all-10'><?php echo img("assets/img/events/$event->evt_img"); ?></div>
+        <div class="span9 opacity-7 white padding-all-10"><?php echo $event->evt_venue; ?></div>
+        <div class="span9 opacity-5 white padding-all-10"><?php echo $event->evt_desc; ?></div>
     </div>
     <?php endforeach; ?>
 </div>
+
+
--- application/views/events.php
+++ application/views/events.php
@@ -1,22 +1,20 @@
 <div class="span9">
-    <div class='row'>
-        <div class="title_bar_white span9">Events</div>
+
+        <div class="title_bar_white ">Events</div>
         <?php foreach($all_events as $event): ?>
-        <div class="opacity-5 span9">
+        <div class="opacity-5 span17 file-hover ">
 
-            <div class="span1"><img src="img/events/<?php echo $event->evt_img; ?>" alt="" /></div>
+            <div class="span18 padding-all-5"><?php echo img("assets/img/events/$event->evt_img"); ?></div>
 
-            <div class="span1"><?php echo img("assets/img/events/$event->evt_img"); ?></div>
+            <div class="span19 white ">
+                <div class="heading3 padding-all-5"><?php echo $event->evt_name; ?></div>
 
-            <div class="span7 white ">
-                <div class="heading2"><?php echo $event->evt_name; ?></div>
-                <p class='lighter font-12'><?php echo $event->evt_desc; ?></p>
                 <span class='lighter font-13'><?php echo $event->evt_date . " | " . $event->evt_venue;?></span>
-                <a href="<?php echo 'event_item?id='.$event->evt_id; ?>" class="btn btn-default btn-small pull-right"> Read More</a>
+                <a href="<?php echo 'event_item?id='.$event->evt_id; ?>" class="btn btn-default btn-small pull-right margin-bottom-5 margin-bottom-5"> Read More</a>
             </div>
         </div>
         <div class='span9'></div>
         <?php endforeach; ?>
-    </div>
+
     
 </div>
